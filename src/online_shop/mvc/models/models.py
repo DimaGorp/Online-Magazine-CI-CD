436a64a06ca04805d3cde66810b65bd074363dc0
PanLipton <panskillsasda@gmail.com>
2024-05-25 23:07:01 +0300

    update product model

--- src/online_shop/mvc/models/models.py
+++ src/online_shop/mvc/models/models.py
@@ -6,6 +6,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/')
+    rating = models.DecimalField(max_digits=2, decimal_places=1, default=0.0)  # Нове поле для рейтингу
+    specifications = models.TextField(blank=True)  # Нове поле для специфікацій
 
     def __str__(self):
         return self.name
